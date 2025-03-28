db6e4db7239624458f2aeef0e42c26b9cd9a6b17
MF <mfregwork21@gmail.com>
2025-03-27 20:01:03 -0700

    Added services section

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,4 @@
-import { assets } from '@/assets/assets'
+import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -15,7 +15,26 @@ const About = () => {
             <div className='flex-1'>
                 <p className='mb-10 max-w-2xl font-Ovo'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eaque qui hic fugit molestias totam laboriosam ad aspernatur deleniti corrupti est assumenda quo, optio eveniet officiis eos voluptas, maiores, asperiores aut? Exercitationem pariatur repellat earum ipsam. Quasi, molestiae? Quasi voluptas ex, expedita dolores minima impedit iusto excepturi vitae laboriosam repudiandae?</p>
 
-                <ul></ul>
+                <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6'>
+                    {infoList.map(({icon, iconDark, title, description}, index) => (
+                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black' key={index}
+                        >
+                            <Image className='w-7 mt-3' src={icon} alt={title}/>
+                            <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                            <p className='text-gray-600 text-sm'>{description}</p>
+                        </li>
+                    ))}
+                </ul>
+
+                <h4 className='my-6 text-gray-700 font-Ovo'>Tools I use</h4>
+
+                <ul className='flex items-center gap-3 sm:gap-5'>
+                    {toolsData.map((tool,index) => (
+                        <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
+                            <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     </div>
