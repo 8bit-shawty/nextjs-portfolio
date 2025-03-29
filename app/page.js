dfb37dcbeab245cc22e61ac9c3fa752f64f95b2d
MF <mfregwork21@gmail.com>
2025-03-28 20:37:41 -0700

    Added darkmode and lightmode functionality

--- app/page.js
+++ app/page.js
@@ -7,8 +7,20 @@ import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const[isDarkMode, setIsDarkMode] = useState(false)
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  },[isDarkMode])
   return (
     <>
       <Navbar/>
