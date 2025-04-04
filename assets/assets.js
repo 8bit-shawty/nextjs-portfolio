9a00cae86f2b585021249eb9ee1dc98014db490a
MF <mfregwork21@gmail.com>
2025-04-04 16:09:16 -0700

    Updated photos

--- assets/assets.js
+++ assets/assets.js
@@ -35,6 +35,10 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import max_logo from './max_logo.png';
+import max_logo_dark from './max_logo_dark.png';
+import sm_img from './sm-img.png';
+import img_lg from './img_lg.png'
 
 export const assets = {
     user_image,
@@ -73,7 +77,11 @@ export const assets = {
     right_arrow,
     send_icon,
     right_arrow_bold,
-    right_arrow_bold_dark
+    right_arrow_bold_dark,
+    max_logo,
+    max_logo_dark,
+    sm_img,
+    img_lg
 };
 
 export const workData = [
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
 
         <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white/50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/10" : ""}`}>
             <a href='#top'>
-                <Image src={isDarkMode? assets.logo_dark : assets.logo} className='w-28 cursor-pointer mr-14' alt=''/>
+                <Image src={isDarkMode? assets.max_logo_dark : assets.max_logo} className='w-28 cursor-pointer mr-14' alt=''/>
             </a>
 
             <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "": "bg-white/50 shadow-sm dark:border dark:border-white/50 dark:bg-transparent"}`}>
